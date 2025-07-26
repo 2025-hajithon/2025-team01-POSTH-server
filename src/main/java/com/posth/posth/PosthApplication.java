cf41e48bcb8d204c19773931e86751b196e82457
kckc0608 <kckc0608@naver.com>
2025-07-26 17:48:28 +0900

    feat: init project

---
+++ src/main/java/com/posth/posth/PosthApplication.java
@@ -0,0 +1,13 @@
+package com.posth.posth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/posth/posth/PosthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posth.posth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
