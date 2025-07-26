014cfc745302c02b8d3c5c3faaed82b577522734
kckc0608 <kckc0608@naver.com>
2025-07-26 19:19:08 +0900

    chore: 미사용코드 삭제

--- src/main/java/com/posth/posth/global/util/JwtUtil.java
+++ src/main/java/com/posth/posth/global/util/JwtUtil.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Component
