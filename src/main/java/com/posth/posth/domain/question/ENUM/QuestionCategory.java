0d6a8d38d38a519b83cef4201babc5cce878c1bc
lejuho <zedd1324@gmail.com>
2025-07-27 01:42:47 +0900

    chore : 답변할 질문 더 이상 없을 시 예외 대신 200

--- src/main/java/com/posth/posth/domain/question/ENUM/QuestionCategory.java
+++ src/main/java/com/posth/posth/domain/question/ENUM/QuestionCategory.java
@@ -1,13 +1,24 @@
 package com.posth.posth.domain.question.ENUM;
 
 public enum QuestionCategory {
-    공부,
-    연애,
-    진로,
-    취업,
-    직장,
-    인간관계,
-    결혼,
-    가족,
-    기타;
+    STUDY("공부"),
+    ROMANCE("연애"),
+    CAREER("진로"),
+    EMPLOYMENT("취업"),
+    WORKPLACE("직장"),
+    RELATIONSHIP("인간관계"),
+    MARRIAGE("결혼"),
+    FAMILY("가족"),
+    ETC("기타"),
+    SELF("자기자신");
+
+    private final String description;
+
+    QuestionCategory(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
 }
