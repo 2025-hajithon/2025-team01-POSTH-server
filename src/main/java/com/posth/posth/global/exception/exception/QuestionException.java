80eb93aa304ba3260f84b56062ff01f379299f3f
kckc0608 <kckc0608@naver.com>
2025-07-27 04:13:34 +0900

    feat: question exception 처리

---
+++ src/main/java/com/posth/posth/global/exception/exception/QuestionException.java
@@ -0,0 +1,7 @@
+package com.posth.posth.global.exception.exception;
+
+public class QuestionException extends DomainException {
+    public QuestionException(String message) {
+        super(message);
+    }
+}
